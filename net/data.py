9e126355dbead48772775b9041a9b1e331773ee5
Ign0reLee <dlwpdud@gmail.com>
2020-12-27 23:22:42 +0900

    Update Some

--- net/data.py
+++ net/data.py
@@ -5,6 +5,9 @@ from nltk import word_tokenize
 from collections import Counter
 
 def prepare_question(question_data):
+    """
+    Prepare MS COCO VQA V1 Question Data
+    """
     
     questions = [q['question'] for q in question_data['questions']]
     for question in questions:
@@ -12,8 +15,27 @@ def prepare_question(question_data):
         yield question
 
 def prepare_answer(answer_data):
+    """
+    Prepare MS COCO VQA V1 Annotation Data
+    """
     
     answers = [a['multiple_choice_answer'] for a in answer_data['annotations']]
     for answer in answers:
         answer = word_tokenize(answer.lower())
         yield answer
+
+def Check_Data(questions, answers):
+    """
+    Check right Pair Data Input
+    """
+    qa_pairs = list(zip(questions['questions'],answers['annotations']))
+    assert all(q['question_id'] == a['question_id'] for q, a in qa_pairs), 'Questions not aligned with answer'
+    assert all(q['image_id'] == a['image_id'] for q, a in qa_pairs), 'Image id of question and answer don\'t match'
+    assert questions['data_type'] == answers['data_type'], 'Mismatched data types'
+    assert questions['data_subtype'] == answers['data_subtype'], 'Mismatched data subtypes'
+
+def make_max_length(data):
+    """
+    Find Max Length of data
+    """
+    return max(map(len, data))
---
+++ net/model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import tensorflow.keras as k
+
+from tensorflow.keras import layers
+
+
+class VQA(k.models):
+
+    def __init__(self):
